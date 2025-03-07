94d5fa084c1174d84dea5468ff76b809a7e0a890
mbillings985 <58754938+mbillings985@users.noreply.github.com>
2025-03-06 18:00:39 -0600

    fixed config and added dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {ignores: ["node_modules", "dist"]},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
